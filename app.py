ebc89ff2707cd1bbbc043acabc5a6022383e39e1
Justin <Sgjustino@users.noreply.huggingface.co>
2024-07-25 15:21:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def chat_fn(user_input, chat_history, follow_up_question=""):
 
     stream = client.chat.completions.create(
         messages=messages,
-        model="llama3-70b-8192",
+        model="llama3-8b-8192",
         temperature=0.7,
         max_tokens=500,
         top_p=1,
